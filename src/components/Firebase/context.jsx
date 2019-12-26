7cfb6a9b85ddee1167da2b274f8a534ca0b1024f
calthejuggler <calthejuggler@gmail.com>
2019-12-26 20:54:58 +0000

    Made higher order component for Firebase

--- src/components/Firebase/context.jsx
+++ src/components/Firebase/context.jsx
@@ -1,5 +1,11 @@
-import {createContext} from 'react';
+import React, {createContext} from 'react';
 
 const FirebaseContext = createContext(null);
+
+export const withFirebase = Component => props => (
+    <FirebaseContext.Consumer>
+        {firebase => <Component {...props} firebase={firebase} />}
+    </FirebaseContext.Consumer>
+)
 
 export default FirebaseContext;
