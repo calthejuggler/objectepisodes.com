fa47cb18cc5e30a90c017a33be78e5834cf29b96
calthejuggler <calthejuggler@gmail.com>
2019-12-26 22:04:42 +0000

    Added Forum Route

---
+++ src/components/Pages/Forum/Forum.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Forum = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Login } from './components/Pages/Login/Login';
 import { Register } from './components/Pages/Register/Register';
 import Forgot from './components/Pages/Forgot/Forgot';
+import Forum from './components/Pages/Forum/Forum';
 
 const routes = [
 	{
@@ -17,6 +18,11 @@ const routes = [
 		name: 'Forgot',
 		path: '/forgot',
 		component: Forgot,
+	},
+	{
+		name: 'Forum',
+		path: '/forum',
+		component: Forum,
 	},
 ];
 
