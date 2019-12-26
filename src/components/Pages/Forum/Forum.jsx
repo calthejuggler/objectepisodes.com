7f8b8499bc873d0b7e51f10eaa76c6cd8084c47b
calthejuggler <calthejuggler@gmail.com>
2019-12-26 22:38:44 +0000

    Added basic categories functionality

--- src/components/Pages/Forum/Forum.jsx
+++ src/components/Pages/Forum/Forum.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react';
+import { withFirebase } from '../../Firebase/context';
 
-export const Forum = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Forum = props => {
+	const [categories, setCategories] = useState([]);
+	useLayoutEffect(() => {
+		props.firebase.db
+			.collection('forum')
+			.get()
+			.then(snap =>
+				snap.forEach(category => {
+					setCategories(prev => [...prev, category.ref.id]);
+				})
+			);
+		return () => {};
+	}, [props.firebase]);
+	return (
+		<div className='text-center'>
+			<h3>Forum</h3>
+			Choose a category...
+			<table>
+				{categories.map(category => (
+					<tr key={category}>
+						<td>{category}</td>
+					</tr>
+				))}
+			</table>
+		</div>
+	);
+};
+
+export default withFirebase(Forum);
