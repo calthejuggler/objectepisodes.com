0e8a876dba6a955a0514c0b1a93f3eeead316716
calthejuggler <calthejuggler@gmail.com>
2019-12-26 15:03:54 +0000

    Added header and login components (EMPTY)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Login } from './components/Pages/Login/Login';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Login />
+		</div>
+	);
 }
 
 export default App;
