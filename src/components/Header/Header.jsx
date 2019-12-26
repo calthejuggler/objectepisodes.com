7731ceeb71982abcd7384b608f2e69ec80113929
calthejuggler <calthejuggler@gmail.com>
2019-12-26 21:56:52 +0000

    Added account area to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 
 import logo from '../../images/objectepisodes_logo.jpg';
+import { withRouter } from 'react-router-dom';
 
-export const Header = () => {
+const Header = props => {
+	const { user } = props;
 	return (
-		<nav className='navbar navbar-light d-block'>
-			<a href='/' className='navbar-brand d-block'>
-				<img
-					src={logo}
-					alt='Object Episodes Logo'
-					className='img-fluid d-block mx-auto'
-                    id='header-logo'
-				/>
-			</a>
-		</nav>
+		<>
+			<nav className='navbar navbar-light d-block'>
+				<a href='/' className='navbar-brand d-block'>
+					<img
+						src={logo}
+						alt='Object Episodes Logo'
+						className='img-fluid d-block mx-auto'
+						id='header-logo'
+					/>
+				</a>
+			</nav>
+			<nav className='navbar navbar-light d-block'>
+				{user ? (
+					<div className="text-center">
+						Logged in as <b>{user.displayName}</b><br/><button className="btn btn-link btn-sm" onClick={()=>{props.firebase.doSignOut()}}>Sign Out?</button>
+					</div>
+				) : (
+					<>
+						<div className='btn btn-primary' onClick={()=>props.history.push('/login')}>Login</div>
+						<div className='btn btn-secondary' onClick={()=>props.history.push('/register')}>Register</div>
+					</>
+				)}
+			</nav>
+		</>
 	);
 };
+
+export default withRouter(Header);
