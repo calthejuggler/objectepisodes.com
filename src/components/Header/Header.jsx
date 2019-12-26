019c7c7e898d6edb77a669881b687277904bf7eb
calthejuggler <calthejuggler@gmail.com>
2019-12-26 18:46:05 +0000

    Added logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import logo from '../../images/objectepisodes_logo.jpg';
 
 export const Header = () => {
-    return (
-        <div>
-            Header
-        </div>
-    )
-}
+	return (
+		<nav className='navbar navbar-light d-block'>
+			<a href='/' className='navbar-brand d-block'>
+				<img
+					src={logo}
+					alt='Object Episodes Logo'
+					className='img-fluid d-block mx-auto'
+                    id='header-logo'
+				/>
+			</a>
+		</nav>
+	);
+};
