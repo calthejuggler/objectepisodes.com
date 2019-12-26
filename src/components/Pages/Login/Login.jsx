0df41a38be87e762906cfa7049b484ce25897727
calthejuggler <calthejuggler@gmail.com>
2019-12-26 19:03:17 +0000

    Made Login Page mobile Friendly

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -4,7 +4,7 @@ import { LoginForm } from './components/LoginForm';
 export const Login = () => {
 	return (
 		<div className='row'>
-			<div className='col-6 offset-3'>
+			<div className='col-12 col-md-6 offset-md-3'>
 				<div className='card'>
 					<div className='card-body text-center'>
 						<h3>Login</h3>
