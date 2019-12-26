705f4b7f078f349ba2a1bd335f052373f98baaee
calthejuggler <calthejuggler@gmail.com>
2019-12-26 19:02:07 +0000

    Added states to LoginForm

--- src/components/Pages/Login/components/LoginForm.jsx
+++ src/components/Pages/Login/components/LoginForm.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const LoginForm = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const handleLoginSubmit = e => {
+		e.preventDefault();
+	};
 	return (
-		<form className='text-left'>
+		<form className='text-left' onSubmit={handleLoginSubmit}>
 			<a href='/register' className='mx-auto d-block mt-3 text-center'>
 				Don't have an account? Register here.
 			</a>
 			<div className='form-group'>
 				<label htmlFor='email'>Email:</label>
-				<input type='email' className='form-control' />
+				<input
+					type='email'
+					className='form-control'
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+				/>
 			</div>
 			<div className='form-group'>
 				<label htmlFor='password'>Password:</label>
-				<input type='password' className='form-control' />
+				<input
+					type='password'
+					className='form-control'
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+				/>
 				<small className='ml-auto'>
 					<a href='/forgot'>Forgot your password?</a>
 				</small>
