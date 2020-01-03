58842d1e5d24a4e4492142512ab590aca20576a4
calthejuggler <calthejuggler@gmail.com>
2020-01-03 21:30:29 +0000

    Started DashForum table

--- src/components/Pages/Dashboard/components/DashForum.jsx
+++ src/components/Pages/Dashboard/components/DashForum.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
+import {withFirebase} from '../../../Firebase/context'
 
 const DashForum = props => {
+	const {history,firebase} = props
 	return (
 		<div className='row'>
 			<div className='col-12 col-md-4'>
@@ -9,10 +11,20 @@ const DashForum = props => {
 					<div className='card-body text-center'>
 						<button
 							className='btn btn-link'
-							onClick={() => props.history.replace('/forum')}>
+							onClick={() => history.replace('/forum')}>
 							<h4 className='card-title'>Forum</h4>
 						</button>
-						<p>Hottest topics right now</p>
+						<p>Most liked topics</p>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">
+								<div className="row">
+									<div className="col-3"><b>Title</b></div>
+									<div className="col-3"><b>User</b></div>
+									<div className="col-3"><b>Date</b></div>
+									<div className="col-3"><b>Likes</b></div>
+								</div>
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
@@ -20,4 +32,4 @@ const DashForum = props => {
 	);
 };
 
-export default withRouter(DashForum);
+export default withFirebase(withRouter(DashForum));
