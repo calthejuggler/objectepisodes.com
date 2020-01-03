88dddf43bc98a5af911abe837224048689391ff3
calthejuggler <calthejuggler@gmail.com>
2020-01-03 14:06:53 +0000

    Added user link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import logo from '../../images/objectepisodes_logo.jpg';
 import { withRouter } from 'react-router-dom';
 
 const Header = props => {
-	const { user } = props;
+	const { user, firebase } = props;
+	const [userData, setUserData] = useState(null);
+	useEffect(() => {
+		if (user) {
+			firebase.db
+				.collection('users')
+				.doc(user.uid)
+				.get()
+				.then(userSnap => {
+					setUserData(userSnap.data());
+				});
+		}
+		return () => {};
+	});
 	return (
 		<>
 			<nav className='navbar navbar-light d-block'>
@@ -18,14 +31,33 @@ const Header = props => {
 				</a>
 			</nav>
 			<nav className='navbar navbar-light d-block'>
-				{user ? (
-					<div className="text-center">
-						Logged in as <b>{user.displayName}</b><br/><button className="btn btn-link btn-sm" onClick={()=>{props.firebase.doSignOut()}}>Sign Out?</button>
+				{userData ? (
+					<div className='text-center'>
+						Logged in as{' '}
+						<a href={'#/user/' + userData.username}>
+							{user.displayName}
+						</a>
+						<br />
+						<button
+							className='btn btn-link btn-sm'
+							onClick={() => {
+								props.firebase.doSignOut();
+							}}>
+							Sign Out?
+						</button>
 					</div>
 				) : (
-					<div className="d-flex justify-content-center">
-						<div className='btn btn-primary mr-3' onClick={()=>props.history.push('/login')}>Login</div>
-						<div className='btn btn-secondary ml-3' onClick={()=>props.history.push('/register')}>Register</div>
+					<div className='d-flex justify-content-center'>
+						<div
+							className='btn btn-primary mr-3'
+							onClick={() => props.history.push('/login')}>
+							Login
+						</div>
+						<div
+							className='btn btn-secondary ml-3'
+							onClick={() => props.history.push('/register')}>
+							Register
+						</div>
 					</div>
 				)}
 			</nav>
