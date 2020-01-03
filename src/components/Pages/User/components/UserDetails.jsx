57d9c69b6a5ad0bb3eae271bd16a2e2a0506cf37
calthejuggler <calthejuggler@gmail.com>
2020-01-03 12:21:44 +0000

    Prettier code

--- src/components/Pages/User/components/UserDetails.jsx
+++ src/components/Pages/User/components/UserDetails.jsx
@@ -5,11 +5,30 @@ const UserDetails = props => {
 	return (
 		<div className='card'>
 			<div className='card-body'>
-				<ul className="list-unstyled">
-					<li>Name: <b>{userData.data().firstname + ' ' + userData.data().lastname}</b></li>
-					<li>Username: <b>{userData.data().username}</b></li>
-					<li>Account Created: <b>{userData.data().created.toDate().toDateString()}</b></li>
-					<li>Forum Posts: <b>{userData.data().forumPosts}</b></li>
+				<ul className='list-unstyled'>
+					<li>
+						Name:{' '}
+						<b>
+							{userData.data().firstname +
+								' ' +
+								userData.data().lastname}
+						</b>
+					</li>
+					<li>
+						Username: <b>{userData.data().username}</b>
+					</li>
+					<li>
+						Account Created:{' '}
+						<b>
+							{userData
+								.data()
+								.created.toDate()
+								.toDateString()}
+						</b>
+					</li>
+					<li>
+						Forum Posts: <b>{userData.data().forumPosts}</b>
+					</li>
 				</ul>
 			</div>
 		</div>
