2f9d2f83e8f268d6a2c53af9665d92b752b5ac0a
calthejuggler <calthejuggler@gmail.com>
2020-01-03 21:17:07 +0000

    Reformatted and fixed multi reading

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,51 +17,53 @@ const Header = props => {
 				});
 		}
 		return () => {};
-	});
+	}, [firebase.db, user]);
 	return (
-		<>
-			<nav className='navbar navbar-light d-block'>
-				<a href='/' className='navbar-brand d-block'>
-					<img
-						src={logo}
-						alt='Object Episodes Logo'
-						className='img-fluid d-block mx-auto'
-						id='header-logo'
-					/>
-				</a>
-			</nav>
-			<nav className='navbar navbar-light d-block'>
-				{userData ? (
-					<div className='text-center'>
-						Logged in as{' '}
-						<a href={'#/user/' + userData.username}>
-							{user.displayName}
-						</a>
-						<br />
-						<button
-							className='btn btn-link btn-sm'
-							onClick={() => {
-								props.firebase.doSignOut();
-							}}>
-							Sign Out?
-						</button>
-					</div>
-				) : (
-					<div className='d-flex justify-content-center'>
-						<div
-							className='btn btn-primary mr-3'
-							onClick={() => props.history.push('/login')}>
-							Login
+		<nav className='navbar navbar-light d-block'>
+			<div className='row align-items-center'>
+				<div className='col-12 col-md-4 offset-md-4'>
+					<a href='/' className='navbar-brand d-block'>
+						<img
+							src={logo}
+							alt='Object Episodes Logo'
+							className='img-fluid d-block mx-auto'
+							id='header-logo'
+						/>
+					</a>
+				</div>
+				<div className='col-12 col-md-4'>
+					{userData ? (
+						<div className='text-center'>
+							Logged in as{' '}
+							<a href={'#/user/' + userData.username}>
+								{user.displayName}
+							</a>
+							<br />
+							<button
+								className='btn btn-link btn-sm'
+								onClick={() => {
+									props.firebase.doSignOut();
+								}}>
+								Sign Out?
+							</button>
 						</div>
-						<div
-							className='btn btn-secondary ml-3'
-							onClick={() => props.history.push('/register')}>
-							Register
+					) : (
+						<div className='d-flex justify-content-center'>
+							<div
+								className='btn btn-primary mr-3'
+								onClick={() => props.history.push('/login')}>
+								Login
+							</div>
+							<div
+								className='btn btn-secondary ml-3'
+								onClick={() => props.history.push('/register')}>
+								Register
+							</div>
 						</div>
-					</div>
-				)}
-			</nav>
-		</>
+					)}
+				</div>
+			</div>
+		</nav>
 	);
 };
 
