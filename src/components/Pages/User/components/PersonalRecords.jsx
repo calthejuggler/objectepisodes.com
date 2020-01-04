4bd07dd8c53e499055aab3d3cca8a8a20e408b13
calthejuggler <calthejuggler@gmail.com>
2020-01-04 19:02:15 +0000

    Ordered records

--- src/components/Pages/User/components/PersonalRecords.jsx
+++ src/components/Pages/User/components/PersonalRecords.jsx
@@ -11,6 +11,7 @@ const PersonalRecords = props => {
 			firebase.db
 				.collection('records')
 				.where('userID', '==', userData.id)
+				.orderBy('recorded', 'desc')
 				.onSnapshot(personalBestsSnap => {
 					setPersonalBests([]);
 					personalBestsSnap.forEach(personalBest =>
