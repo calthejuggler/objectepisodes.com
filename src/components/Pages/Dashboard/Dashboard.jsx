f05d3ab2e1210c1651b2a9e58c4da16ee9f43288
calthejuggler <calthejuggler@gmail.com>
2020-01-04 17:09:49 +0000

    Included DashRecords

--- src/components/Pages/Dashboard/Dashboard.jsx
+++ src/components/Pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import DashForum from './components/DashForum';
+import DashRecords from './components/DashRecords';
 
 const Dashboard = () => {
 	return (
-		<div className='row'>
-			<div className='col-12 col-md-4'>
-				<DashForum />
+		<>
+			<div className='row'>
+				<div className='col-12 col-md-4'>
+					<DashForum />
+				</div>
 			</div>
-		</div>
+			<div className='row'>
+				<div className='col-12 col-md-4'>
+					<DashRecords />
+				</div>
+			</div>
+		</>
 	);
 };
 
