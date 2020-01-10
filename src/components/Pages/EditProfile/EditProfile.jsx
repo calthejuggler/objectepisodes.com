1c6479318e11e87200515d0ffa2c8a540a1e3447
calthejuggler <calthejuggler@gmail.com>
2020-01-10 16:04:40 +0000

    Fixed UserDataLoad

--- src/components/Pages/EditProfile/EditProfile.jsx
+++ src/components/Pages/EditProfile/EditProfile.jsx
@@ -1,29 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import { withFirebase } from '../../Firebase/context';
 
 import EditProfileNav from './components/EditProfileNav';
 import EditProfilePersonalForm from './components/EditProfilePersonalForm';
+import { withAuth } from '../../Session/withAuth';
 
 const EditProfile = props => {
-	const { firebase } = props;
+	const { firebase, user } = props;
 	const [currentSetting, setCurrentSetting] = useState('personal');
 
 	const [firstname, setFirstname] = useState('');
 	const [lastname, setLastname] = useState('');
 	const [email, setEmail] = useState('');
 
+	const saveChanges = e => {
+		e.preventDefault();
+		firebase.db
+			.collection('users')
+			.doc(user.uid)
+			.update({
+				firstname: firstname,
+				lastname: lastname,
+				email: email,
+			});
+	};
+
+	const loadUserInfo = useCallback(
+		e => {
+			if (e) e.preventDefault();
+			if (user) {
+				firebase
+					.getUserDataFromUID(firebase.auth.currentUser.uid)
+					.then(userSnap => {
+						setEmail(userSnap.data().email);
+						setFirstname(userSnap.data().firstname);
+						setLastname(userSnap.data().lastname);
+					});
+			}
+		},
+		[firebase, user]
+	);
+
 	useEffect(() => {
-		if (firebase.auth.currentUser) {
-			firebase
-				.getUserDataFromUID(firebase.auth.currentUser.uid)
-				.then(userSnap => {
-					setEmail(userSnap.data().email);
-					setFirstname(userSnap.data().firstname);
-					setLastname(userSnap.data().lastname);
-				});
-		}
-	}, [firebase.auth.currentUser, firebase]);
+		loadUserInfo();
+	}, [loadUserInfo]);
 
 	return (
 		<div className='row'>
@@ -41,10 +62,12 @@ const EditProfile = props => {
 					setFirstname={setFirstname}
 					setLastname={setLastname}
 					setEmail={setEmail}
+					saveChanges={saveChanges}
+					loadUserInfo={loadUserInfo}
 				/>
 			</div>
 		</div>
 	);
 };
 
-export default withFirebase(EditProfile);
+export default withAuth(withFirebase(EditProfile));
