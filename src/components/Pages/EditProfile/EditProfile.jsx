a5dba30f8df69cda25c8f6e67cc1cf2149ebf513
calthejuggler <calthejuggler@gmail.com>
2020-01-10 15:27:49 +0000

    Compartmentalised and added firebase search

--- src/components/Pages/EditProfile/EditProfile.jsx
+++ src/components/Pages/EditProfile/EditProfile.jsx
@@ -1,104 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { withFirebase } from '../../Firebase/context';
+
+import EditProfileNav from './components/EditProfileNav';
+import EditProfilePersonalForm from './components/EditProfilePersonalForm';
 
 const EditProfile = props => {
+	const { firebase } = props;
 	const [currentSetting, setCurrentSetting] = useState('personal');
+
+	const [firstname, setFirstname] = useState('');
+	const [lastname, setLastname] = useState('');
+	const [email, setEmail] = useState('');
+
+	useEffect(() => {
+		if (firebase.auth.currentUser) {
+			firebase
+				.getUserDataFromUID(firebase.auth.currentUser.uid)
+				.then(userSnap => {
+					setEmail(userSnap.data().email);
+					setFirstname(userSnap.data().firstname);
+					setLastname(userSnap.data().lastname);
+				});
+		}
+	}, [firebase.auth.currentUser, firebase]);
 
 	return (
 		<div className='row'>
-			<div className='col-4'>
-				<div className='card'>
-					<div className='card-body'>
-						<h4 className='card-title text-center'>
-							Edit Navigation
-						</h4>
-						<ul className='list-group'>
-							<li
-								className='list-group-item'
-								onClick={() => setCurrentSetting('personal')}>
-								Personal Information
-							</li>
-							<li
-								className='list-group-item'
-								onClick={() => setCurrentSetting('security')}>
-								Password & Security
-							</li>
-						</ul>
-					</div>
-				</div>
+			<div className='col-12 col-md-4'>
+				<EditProfileNav
+					setCurrentSetting={setCurrentSetting}
+					currentSetting={currentSetting}
+				/>
 			</div>
-			<div className='col-8'>
-				<div className='card'>
-					<div className='card-body'>
-						<h4 className='card-title text-center'>
-							Personal Information
-						</h4>
-						<hr />
-						<h5 className='text-center'>Profile Picture</h5>
-						<div className='row align-items-center'>
-							<div className='col-6'>
-								<img
-									src='https://via.placeholder.com/150x150'
-									alt='Profile pic placeholder'
-									className='rounded-circle d-block mx-auto'
-								/>
-							</div>
-							<div className='col-6'>
-								<div className='form-group'>
-									<label htmlFor='profilepic'>
-										Upload Image
-									</label>
-									<input
-										type='file'
-										name='profilepic'
-										id='profilePic'
-										className='form-control-file'
-									/>
-								</div>
-							</div>
-						</div>
-						<hr />
-						<div className='row'>
-							<div className='col'>
-								<div className='form-group'>
-									<label htmlFor='firstname'>
-										First Name:
-									</label>
-									<input
-										type='text'
-										className='form-control'
-									/>
-								</div>
-							</div>
-							<div className='col'>
-								<div className='form-group'>
-									<label htmlFor='lastname'>Last Name:</label>
-									<input
-										type='text'
-										className='form-control'
-									/>
-								</div>
-							</div>
-						</div>
-						<div className='row'>
-							<div className='col-12'>
-								<div className='form-group'>
-									<label htmlFor='email'>
-										Email Address:
-									</label>
-									<input
-										type='email'
-										name='email'
-										id='email'
-										className='form-control'
-									/>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+			<div className='col-12 mt-2 mt-md-0 col-md-8'>
+				<EditProfilePersonalForm
+					firstname={firstname}
+					lastname={lastname}
+					email={email}
+					setFirstname={setFirstname}
+					setLastname={setLastname}
+					setEmail={setEmail}
+				/>
 			</div>
 		</div>
 	);
 };
 
-export default EditProfile;
+export default withFirebase(EditProfile);
