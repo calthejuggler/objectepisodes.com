b23bc88b777b24379cf8d314d5021be57a74ea78
calthejuggler <calthejuggler@gmail.com>
2020-01-19 01:09:16 +0000

    Formatted text

--- src/components/Admin/components/AdminHeader.jsx
+++ src/components/Admin/components/AdminHeader.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const AdminHeader = props => {
 	return (
 		<nav className='navbar navbar-light bg-danger mb-3'>
-			<div className='row'>
-				<div className='col-12 text-white'>
+			<div className='row w-100 d-block'>
+				<div className='col text-center text-white'>
 					You are an Admin. <a href='#/admin'>Click here</a> for the
 					admin page.
 				</div>
