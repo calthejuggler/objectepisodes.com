0720a6a769d4a4b0e5ec0b7a69ae0a91053376b7
calthejuggler <calthejuggler@gmail.com>
2020-01-19 01:00:03 +0000

    Added admin banner

---
+++ src/components/Admin/components/AdminHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {withAuth} from '../../Session/withAuth'
+
+const AdminHeader = (props) => {
+    const {user} = props
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default withAuth(AdminHeader)
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,74 +20,89 @@ const Header = props => {
 		return () => {};
 	}, [firebase.db, user]);
 	return (
-		<nav className='navbar navbar-light d-block mt-1'>
-			<div className='row align-items-center'>
-				<div className='col-12 col-md-4 order-1 order-md-2'>
-					<a href='/' className='navbar-brand d-block'>
-						<img
-							src={logo}
-							alt='Object Episodes Logo'
-							className='img-fluid d-block mx-auto'
-							id='header-logo'
-						/>
-					</a>
-				</div>
-				<div className='col-12 col-md-4 order-2 order-md-1'>
-					{user && userData ? (
-						<div className='text-center'>
-							{!user.photoURL ? (
-								<img
-									src='https://via.placeholder.com/50x50'
-									alt='profile pic'
-									className='rounded-circle'
-								/>
-							) : (
-								<img
-									src={user.photoURL}
-									alt='profile pic'
-									style={{
-										height: '50px',
-										width: '50px',
-										objectFit: 'cover',
-									}}
-									className='img-fluid rounded-circle'
-								/>
-							)}
-							<p>
-								Logged in as{' '}
-								<a href={'#/user/' + userData.username}>
-									{user.displayName}
-								</a>
-								<br />
-								<button
-									className='btn btn-link btn-sm'
-									onClick={() => {
-										props.firebase.doSignOut();
-									}}>
-									Sign Out?
-								</button>
-							</p>
-						</div>
-					) : (
-						<div className='d-flex justify-content-center'>
-							<div
-								className='btn btn-primary mr-3'
-								onClick={() => props.history.push('/login')}>
-								Login
+		<>
+			<nav className='navbar navbar-light d-block mt-1'>
+				<div className='row align-items-center'>
+					<div className='col-12 col-md-4 order-1 order-md-2'>
+						<a href='/' className='navbar-brand d-block'>
+							<img
+								src={logo}
+								alt='Object Episodes Logo'
+								className='img-fluid d-block mx-auto'
+								id='header-logo'
+							/>
+						</a>
+					</div>
+					<div className='col-12 col-md-4 order-2 order-md-1'>
+						{user && userData ? (
+							<div className='text-center'>
+								{!user.photoURL ? (
+									<img
+										src='https://via.placeholder.com/50x50'
+										alt='profile pic'
+										className='rounded-circle'
+									/>
+								) : (
+									<img
+										src={user.photoURL}
+										alt='profile pic'
+										style={{
+											height: '50px',
+											width: '50px',
+											objectFit: 'cover',
+										}}
+										className='img-fluid rounded-circle'
+									/>
+								)}
+								<p>
+									Logged in as{' '}
+									<a href={'#/user/' + userData.username}>
+										{user.displayName}
+									</a>
+									<br />
+									<button
+										className='btn btn-link btn-sm'
+										onClick={() => {
+											props.firebase.doSignOut();
+										}}>
+										Sign Out?
+									</button>
+								</p>
 							</div>
-							<div
-								className='btn btn-secondary ml-3'
-								onClick={() => props.history.push('/register')}>
-								Register
+						) : (
+							<div className='d-flex justify-content-center'>
+								<div
+									className='btn btn-primary mr-3'
+									onClick={() =>
+										props.history.push('/login')
+									}>
+									Login
+								</div>
+								<div
+									className='btn btn-secondary ml-3'
+									onClick={() =>
+										props.history.push('/register')
+									}>
+									Register
+								</div>
 							</div>
+						)}
+					</div>
+					<div className='col-12 col-md-4 order-3 order-md-3'>
+						<HeaderHFOTD />
+					</div>
+				</div>
+			</nav>
+			{userData && userData.admin && (
+				<nav className='navbar navbar-light bg-danger mb-3'>
+					<div className="row">
+						<div className="col-12 text-white">
+							You are an Admin
 						</div>
-					)}
-				</div>
-				<div className='col-12 col-md-4 order-3 order-md-3'>
-					<HeaderHFOTD />
-				</div>
-			</div>
-		</nav>
+					</div>
+				</nav>
+			)}
+		</>
 	);
 };
 
