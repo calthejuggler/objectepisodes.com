0bf018a15a5a42835c14d2b77e17a3284e2335ce
calthejuggler <calthejuggler@gmail.com>
2020-02-02 18:23:52 +0100

    Added delete buttons and error messages

--- src/components/Pages/Admin/components/ListHFOTD.jsx
+++ src/components/Pages/Admin/components/ListHFOTD.jsx
@@ -4,6 +4,8 @@ import { withFirebase } from '../../../Firebase/context';
 const ListHFOTD = props => {
 	const { firebase } = props;
 	const [facts, setFacts] = useState([]);
+	const [message, setMessage] = useState(null);
+	const [error, setError] = useState(null);
 	useLayoutEffect(() => {
 		return firebase.db
 			.collection('hfotd')
@@ -25,43 +27,74 @@ const ListHFOTD = props => {
 			});
 	}, [firebase.db]);
 	return (
-		<ul className='list-group list-group-flush'>
-			<li className='list-group-item'>
-				<div className='row text-center'>
-					<div className='col-5'>
-						<b>Historical Fact</b>
+		<>
+			{error && <div className='alert alert-danger'>{error}</div>}
+			{message && <div className='alert alert-success'>{message}</div>}
+			<ul className='list-group list-group-flush'>
+				<li className='list-group-item'>
+					<div className='row text-center'>
+						<div className='col-5'>
+							<b>Historical Fact</b>
+						</div>
+						<div className='col-3'>
+							<b>Posted By</b>
+						</div>
+						<div className='col-2'>
+							<b>Shown?</b>
+						</div>
+						<div className='col-2'>
+							<b>Delete</b>
+						</div>
 					</div>
-					<div className='col-4'>
-						<b>Posted By</b>
-					</div>
-					<div className='col-3'>
-						<b>Shown?</b>
-					</div>
-				</div>
-			</li>
-			{facts.length > 0
-				? facts.map(fact => (
-						<li className='list-group-item' key={fact.factData.id}>
-							<div className='row text-center'>
-								<div className='col-5'>
-									{fact.factData.data().fact}
+				</li>
+				{facts.length > 0
+					? facts.map(fact => (
+							<li
+								className='list-group-item'
+								key={fact.factData.id}>
+								<div className='row text-center'>
+									<div className='col-5'>
+										{fact.factData.data().fact}
+									</div>
+									<div className='col-3'>
+										<a href={'#/users/' + fact.user.id}>
+											{fact.user.data().username}
+										</a>
+									</div>
+									<div className='col-2'>
+										{fact.factData.data().toBeShown <=
+										new Date().valueOf()
+											? 'Yes'
+											: 'No'}
+									</div>
+									<div className='col-2'>
+										<button
+											className='btn btn-danger'
+											onClick={e => {
+												e.preventDefault();
+												setMessage(null);
+												firebase.db
+													.collection('hfotd')
+													.doc(fact.factData.id)
+													.delete()
+													.then(res => {
+														setMessage(
+															'HFOTD successfully deleted!'
+														);
+													})
+													.catch(e =>
+														setError(e.message)
+													);
+											}}>
+											Delete
+										</button>
+									</div>
 								</div>
-								<div className='col-4'>
-									<a href={'#/users/' + fact.user.id}>
-										{fact.user.data().username}
-									</a>
-								</div>
-								<div className='col-3'>
-									{fact.factData.data().toBeShown <=
-									new Date().valueOf()
-										? 'Yes'
-										: 'No'}
-								</div>
-							</div>
-						</li>
-				  ))
-				: null}
-		</ul>
+							</li>
+					  ))
+					: null}
+			</ul>
+		</>
 	);
 };
 
