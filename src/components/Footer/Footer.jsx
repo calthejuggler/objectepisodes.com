9d833436092bf2be76859b8db876db907217a5e9
calthejuggler <calthejuggler@gmail.com>
2020-02-03 00:25:05 +0100

    Added disabled links and donate area

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,38 +2,89 @@ import React from 'react';
 
 export const Footer = () => {
 	return (
-		<footer className='bg-secondary text-white text-center'>
-			<div className='row align-items-center justify-content-center'>
-				<div className='col-6'>
+		<footer className='bg-dark text-white text-center'>
+			<div className='row align-items-center justify-content-center py-3'>
+				<div className='col-4'>
 					<ul className='navbar-nav'>
 						<li>
-							<a href='' className='text-white'>
+							<button
+								className='btn btn-link'
+								type='button'
+								disabled>
 								Privacy Policy
-							</a>
+							</button>
 						</li>
 						<li>
-							<a href='' className='text-white'>
+							<button
+								className='btn btn-link'
+								type='button'
+								disabled>
 								Terms of Service
-							</a>
+							</button>
 						</li>
 						<li>
-							<a href='' className='text-white'>
+							<button
+								className='btn btn-link'
+								type='button'
+								disabled>
 								Sitemap
-							</a>
+							</button>
 						</li>
 					</ul>
 				</div>
-				<div className='col-6'>
+				<div className='col-4'>
+					<ul className='navbar-nav text-muted'>
+						<li>
+							Consider donating to support objectepisodes.com!
+						</li>
+						<li>
+							<small>
+								It costs 12.99EUR/month to run this website.
+								<br />
+								Wanna buy a round?
+							</small>
+						</li>
+						<li>
+							<button disabled className='btn btn-disabled mt-2'>
+								Donate
+							</button>
+						</li>
+					</ul>
+				</div>
+				<div className='col-4'>
 					<ul className='navbar-nav'>
 						<li>
-							<button className='btn btn-primary'>Donate</button>
+							<button
+								className='btn btn-link'
+								type='button'
+								disabled>
+								About this Site
+							</button>
 						</li>
 						<li>
-							<a href='' className='text-white'>
-								About this Site
-							</a>
+							<button
+								className='btn btn-link'
+								type='button'
+								disabled>
+								Give Feedback
+							</button>
 						</li>
-						<li></li>
+						<li>
+							<button
+								className='btn btn-link'
+								type='button'
+								disabled>
+								Want to Help?
+							</button>
+						</li>
+						<li>
+							<button
+								className='btn btn-link'
+								type='button'
+								disabled>
+								Report a Bug
+							</button>
+						</li>
 					</ul>
 				</div>
 			</div>
