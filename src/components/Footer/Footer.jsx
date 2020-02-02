2fde4a3fe6a8c08d27637e17aaa2a7deb7cacbbf
calthejuggler <calthejuggler@gmail.com>
2020-02-02 23:32:03 +0100

    Added placeholder footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
 
 export const Footer = () => {
-    return (
-        <div>
-            Footer
-        </div>
-    )
-}
+	return (
+		<footer className='bg-secondary text-white text-center'>
+			<div className='row align-items-center justify-content-center'>
+				<div className='col-6'>
+					<ul className='navbar-nav'>
+						<li>
+							<a href='' className='text-white'>
+								Privacy Policy
+							</a>
+						</li>
+						<li>
+							<a href='' className='text-white'>
+								Terms of Service
+							</a>
+						</li>
+						<li>
+							<a href='' className='text-white'>
+								Sitemap
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className='col-6'>
+					<ul className='navbar-nav'>
+						<li>
+							<button className='btn btn-primary'>Donate</button>
+						</li>
+						<li>
+							<a href='' className='text-white'>
+								About this Site
+							</a>
+						</li>
+						<li></li>
+					</ul>
+				</div>
+			</div>
+		</footer>
+	);
+};
