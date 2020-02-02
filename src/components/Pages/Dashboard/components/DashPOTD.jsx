270dc228b32236dfe4c6ff4392c812492a92d468
calthejuggler <calthejuggler@gmail.com>
2020-02-02 22:46:58 +0100

    Added working firestore functionality

--- src/components/Pages/Dashboard/components/DashPOTD.jsx
+++ src/components/Pages/Dashboard/components/DashPOTD.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const DashPOTD = () => {
+import { withFirebase } from '../../../Firebase/context';
+
+const DashPOTD = props => {
+	const { firebase } = props;
+	const [potd, setPotd] = useState(null);
+	useEffect(() => {
+		firebase.db
+			.collection('potd')
+			.where('toBeShown', '>=', new Date().setTime(0).valueOf())
+			.where(
+				'toBeShown',
+				'<=',
+				new Date().setDate(new Date().getDate() + 1).valueOf()
+			)
+			.limit(1)
+			.get()
+			.then(snapArray => {
+				firebase
+					.getUserDataFromUID(snapArray.docs[0].data().uploadedBy)
+					.then(user => {
+						setPotd({
+							potd: snapArray.docs[0].data(),
+							user: user.data(),
+						});
+					});
+			});
+	}, [firebase]);
 	return (
 		<div className='card h-100'>
 			<div className='card-body text-center h-100'>
 				<h4 className='card-title text-center'>Photo of the Day</h4>
-				{/* <img
-					src='https://via.placeholder.com/800x400'
-					alt='Placeholder'
-					className='img-fluid'
-				/> */}
-				<b className='d-block m-auto'>*COMING SOON*</b>
+				{potd && (
+					<>
+						<p className='text-center small'>
+							Uploaded By:{' '}
+							<a href={'#/user/' + potd.user.username}>
+								{potd.user.username}
+							</a>
+						</p>
+						<img
+							src={potd.potd.photoUrl}
+							alt='Placeholder'
+							className='img-fluid'
+							style={{ maxHeight: '20rem' }}
+						/>
+					</>
+				)}
 			</div>
 		</div>
 	);
 };
 
-export default DashPOTD;
+export default withFirebase(DashPOTD);
