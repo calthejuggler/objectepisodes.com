6ac85e0ba90d6d48c6795cfd604f647d40922fb3
calthejuggler <calthejuggler@gmail.com>
2020-02-02 16:39:24 +0100

    Took out weird margin

--- src/components/Admin/components/AdminHeader.jsx
+++ src/components/Admin/components/AdminHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AdminHeader = props => {
 	return (
-		<nav className='navbar navbar-light bg-danger mb-3'>
+		<nav className='navbar navbar-light bg-danger'>
 			<div className='row w-100 d-block'>
 				<div className='col text-center text-white'>
 					You are an Admin. <a href='#/admin'>Click here</a> for the
