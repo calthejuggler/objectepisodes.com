37d2e4a658d7c5fbc85fb71d7cec4881287acfa8
calthejuggler <calthejuggler@gmail.com>
2020-02-05 12:53:24 +0100

    Added sortby and sortdirection

--- src/components/Pages/Admin/components/AdminUsers/components/UsersList.jsx
+++ src/components/Pages/Admin/components/AdminUsers/components/UsersList.jsx
@@ -22,35 +22,63 @@ const UsersList = props => {
 			});
 	}, [firebase.db, sortBy, sortDirection]);
 	return (
-		<ul className='list-group-flush'>
-			<li className='list-group-item'>
-				<div className='row text-center'>
-					<div className='col-2'>
-						<b>Pic</b>
-					</div>
-					<div className='col-2'>
-						<b>Username</b>
-					</div>
-					<div className='col-2'>
-						<b>Name</b>
-					</div>
-					<div className='col-3'>
-						<b>Email</b>
-					</div>
-					<div className='col-2'>
-						<b>Created</b>
-					</div>
+		<>
+			<div className='input-group w-50 mx-auto'>
+				<div className='input-group-prepend'>
+					<button
+						className='btn btn-primary'
+						onClick={() => {
+							sortDirection === 'desc'
+								? setSortDirection('asc')
+								: setSortDirection('desc');
+						}}>
+						{sortDirection[0].toUpperCase() +
+							sortDirection.slice(1) +
+							'.'}
+					</button>
 				</div>
-			</li>
-			{userArray !== [] &&
-				userArray.map(user => (
-					<UserRow
-						user={user}
-						key={user.id}
-						setSelectedUser={setSelectedUser}
-					/>
-				))}
-		</ul>
+				<select
+					className='custom-select'
+					value={sortBy}
+					onChange={e => setSortBy(e.target.value)}
+					name='sortBy'>
+					<option value='created'>Added</option>
+					<option value='firstname'>Name</option>
+					<option value='username'>Username</option>
+					<option value='forumPosts'>Forum Posts</option>
+					<option value='likes'>Likes</option>
+				</select>
+			</div>
+			<ul className='list-group-flush'>
+				<li className='list-group-item'>
+					<div className='row text-center'>
+						<div className='col-2'>
+							<b>Pic</b>
+						</div>
+						<div className='col-2'>
+							<b>Username</b>
+						</div>
+						<div className='col-2'>
+							<b>Name</b>
+						</div>
+						<div className='col-3'>
+							<b>Email</b>
+						</div>
+						<div className='col-2'>
+							<b>Created</b>
+						</div>
+					</div>
+				</li>
+				{userArray !== [] &&
+					userArray.map(user => (
+						<UserRow
+							user={user}
+							key={user.id}
+							setSelectedUser={setSelectedUser}
+						/>
+					))}
+			</ul>
+		</>
 	);
 };
 
