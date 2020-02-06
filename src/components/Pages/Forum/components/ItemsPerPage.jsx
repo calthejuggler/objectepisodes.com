fb9220b4461278be7f19f26137fe16cbff77cc95
calthejuggler <calthejuggler@gmail.com>
2020-02-06 22:44:19 +0100

    Compartmentalised items per page

---
+++ src/components/Pages/Forum/components/ItemsPerPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const ItemsPerPage = props => {
+	const { setLoadingState, setItemsPerPage, currentItemsPerPage } = props;
+	return (
+		<div className='form-group'>
+			<label htmlFor='topicsPerPage'>Items/Page: </label>
+			<select
+				name='topicsPerPage'
+				id='topicsPerPage'
+				className='dropdown'
+				value={currentItemsPerPage}
+				onChange={e => {
+					setLoadingState(1);
+					setItemsPerPage(parseInt(e.target.value, 10));
+				}}>
+				<option value={5}>5</option>
+				<option value={10}>10</option>
+				<option value={20}>20</option>
+				<option value={40}>40</option>
+				<option value={80}>80</option>
+				<option value={100}>100</option>
+			</select>
+		</div>
+	);
+};
+
+export default ItemsPerPage;
