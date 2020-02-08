99842d7309a18ccbafc223d6aa8ca042bb14a2a7
calthejuggler <calthejuggler@gmail.com>
2020-02-08 01:21:14 +0100

    Added Hashes

--- src/components/Pages/Dashboard/components/DashNavigation.jsx
+++ src/components/Pages/Dashboard/components/DashNavigation.jsx
@@ -4,16 +4,16 @@ const DashNavigation = () => {
 	return (
 		<ul className='list-group'>
 			<li className='list-group-item'>
-				<a href='/jugglingtools'>Juggling Tools (Coming Soon)</a>
+				<a href='#/jugglingtools'>Juggling Tools (Coming Soon)</a>
 			</li>
 			<li className='list-group-item'>
-				<a href='/jugglingclubs'>Juggling Clubs (Coming Soon)</a>
+				<a href='#/jugglingclubs'>Juggling Clubs (Coming Soon)</a>
 			</li>
 			<li className='list-group-item'>
-				<a href='/jugglingpodcasts'>Juggling Podcasts (Coming Soon)</a>
+				<a href='#/jugglingpodcasts'>Juggling Podcasts (Coming Soon)</a>
 			</li>
 			<li className='list-group-item'>
-				<a href='/events'>Juggling Events (Coming Soon)</a>
+				<a href='#/events'>Juggling Events (Coming Soon)</a>
 			</li>
 		</ul>
 	);
