06ea669e3a3816742a1f0e6ae36526543d7a3917
calthejuggler <calthejuggler@gmail.com>
2020-02-10 00:36:54 +0100

    Made navigation much cleaner

--- src/components/Pages/Dashboard/components/DashNavigation.jsx
+++ src/components/Pages/Dashboard/components/DashNavigation.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
 
+import routes from '../../../../routes';
+import { NavLink } from 'react-router-dom';
+
 const DashNavigation = () => {
 	return (
-		<ul className='list-group'>
-			<li className='list-group-item'>
-				<a href='#/jugglingtools'>Juggling Tools (Coming Soon)</a>
-			</li>
-			<li className='list-group-item'>
-				<a href='#/jugglingclubs'>Juggling Clubs (Coming Soon)</a>
-			</li>
-			<li className='list-group-item'>
-				<a href='#/jugglingpodcasts'>Juggling Podcasts (Coming Soon)</a>
-			</li>
-			<li className='list-group-item'>
-				<a href='#/events'>Juggling Events (Coming Soon)</a>
-			</li>
+		<ul className="list-group list-group-flush">
+			{routes.map(route =>
+				route.hidden ? null : route.inProgress ? (
+					<li
+						className='list-group-item'
+						key={route.name}
+						data-toggle='tooltip'
+						data-placement='bottom'
+						title='Coming Soon!'>
+						<button
+							type='button'
+							className='btn btn-link nav-link disabled d-block mx-auto'>
+							{route.name}
+						</button>
+					</li>
+				) : (
+					<li className='list-group-item' key={route.name}>
+						<NavLink
+							className='nav-link'
+							activeClassName='active'
+							exact
+							to={route.path}>
+							{route.name}
+						</NavLink>
+					</li>
+				)
+			)}
 		</ul>
 	);
 };
-
 export default DashNavigation;
