441e68f053de4838733f9980583d2ffce22ca7d2
calthejuggler <calthejuggler@gmail.com>
2020-03-22 00:59:28 +0100

    Update to send confirmation email onsubscribe

--- src/components/Pages/LandingPage/index.tsx
+++ src/components/Pages/LandingPage/index.tsx
@@ -1,7 +1,9 @@
 import React, { FC, FormEvent } from 'react';
 
+import emailjs from 'emailjs-com';
+
 import logo from '../../../images/objectepisodes_logo.jpg';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { withFirebase } from '../../Firebase/context';
 import Firebase from './../../Firebase/index';
 
@@ -11,11 +13,15 @@ const LandingPage: FC<{ firebase: Firebase }> = props => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 
-	const [betaTester, setBetaTester] = useState(false);
+	const [betaTester, setBetaTester] = useState<boolean>(false);
 	const [error, setError] = useState<null | string>(null);
 	const [message, setMessage] = useState<null | string>(null);
 
-	const handleSubmit = (e:FormEvent) => {
+	useEffect(() => {
+		emailjs.init('user_QdG9REKFW3zQjwPZCQrBF');
+	}, []);
+
+	const handleSubmit = (e: FormEvent) => {
 		e.preventDefault();
 		setError(null);
 		setMessage(null);
@@ -24,7 +30,7 @@ const LandingPage: FC<{ firebase: Firebase }> = props => {
 				.collection('users')
 				.where('email', '==', email)
 				.get()
-				.then((res: any) => {
+				.then((res: firebase.firestore.QuerySnapshot) => {
 					if (res.docs[0]) {
 						setBetaTester(true);
 					} else {
@@ -32,29 +38,56 @@ const LandingPage: FC<{ firebase: Firebase }> = props => {
 							.collection('mailing-list')
 							.where('email', '==', email)
 							.get()
-							.then((waitingListSnap: any) => {
-								if (waitingListSnap.docs[0]) {
-									setError(
-										'You are already subscribed to the mailing list!'
-									);
-								} else {
-									firebase.db
-										.collection('mailing-list')
-										.add({
-											email: email,
-											timestamp: firebase.dbFunc.FieldValue.serverTimestamp()
-										})
-										.then(() => {
-											setMessage(
-												'You have successfully subscribed!'
-											);
-										})
-										.catch((e: { message: string }) =>
-											setError(e.message)
+							.then(
+								(
+									waitingListSnap: firebase.firestore.QuerySnapshot
+								) => {
+									if (waitingListSnap.docs[0]) {
+										setError(
+											'You are already subscribed to the mailing list!'
 										);
+									} else {
+										firebase.db
+											.collection('mailing-list')
+											.add({
+												email: email,
+												timestamp: firebase.dbFunc.FieldValue.serverTimestamp()
+											})
+											.then(
+												(
+													doc: firebase.firestore.DocumentReference
+												) => {
+													setMessage(
+														'You have successfully subscribed! Sending confirmation email...'
+													);
+													emailjs
+														.send(
+															'default_service',
+															'oe_mailing_list',
+															{
+																user_email: email,
+																doc_id: doc.id
+															}
+														)
+														.then(() => {
+															setMessage(
+																'You have successfully subscribed! A confirmation email has been sent to the address that you provided.'
+															);
+														})
+														.catch((e: Error) =>
+															setError(e.message)
+														);
+												}
+											)
+											.catch((e: { message: string }) =>
+												setError(e.message)
+											);
+									}
 								}
-							})
-							.catch((e: { message: string }) => setError(e.message));
+							)
+							.catch((e: { message: string }) =>
+								setError(e.message)
+							);
 					}
 				})
 				.catch((e: { message: string }) => setError(e.message));
