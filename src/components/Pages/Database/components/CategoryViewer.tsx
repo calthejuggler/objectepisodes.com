bcdb7db18241dc3722f20289879c19d0fab92de2
calthejuggler <calthejuggler@gmail.com>
2020-05-10 05:07:15 +0200

    Added search bars

--- src/components/Pages/Database/components/CategoryViewer.tsx
+++ src/components/Pages/Database/components/CategoryViewer.tsx
@@ -4,11 +4,13 @@ import React, {
 	useState,
 	Dispatch,
 	SetStateAction,
+	useCallback,
 } from 'react';
 import { withFirebase } from '../../../Firebase/context';
 import Firebase from '../../../Firebase/config';
 import ItemCard from './ItemCard';
 import BackButton from './BackButton';
+import SearchBar from './../../../elements/SearchBar/SearchBar';
 import routes from '../../../../routes';
 
 const CategoryViewer: FC<{
@@ -19,46 +21,100 @@ const CategoryViewer: FC<{
 	const [dataArray, setDataArray] = useState<
 		firebase.firestore.DocumentData[]
 	>([]);
+	const [displayedData, setDisplayedData] = useState<
+		firebase.firestore.DocumentData[]
+	>([]);
+	const [currentSearch, setCurrentSearch] = useState<string>('');
+
+	const createDataArray = (queryRes: firebase.firestore.QuerySnapshot) => {
+		let newArr: firebase.firestore.DocumentData[] = [];
+		queryRes.docs.forEach((doc) => {
+			newArr.push(doc.data());
+		});
+		return newArr;
+	};
+
+	const searchFilter = useCallback(
+		(propDataArray) => {
+			if (currentSearch !== '') {
+				let newArr: firebase.firestore.DocumentData[] = [];
+				propDataArray.forEach(
+					(docData: firebase.firestore.DocumentData) => {
+						let includesSearchTerm = false;
+						Object.values(docData).forEach((value: any) => {
+							if (
+								typeof value === 'string' &&
+								value
+									.toLowerCase()
+									.includes(currentSearch.toLowerCase())
+							) {
+								includesSearchTerm = true;
+							}
+						});
+						if (includesSearchTerm) {
+							newArr.push(docData);
+						}
+					}
+				);
+				return newArr;
+			} else {
+				return propDataArray;
+			}
+		},
+		[currentSearch]
+	);
+
 	useEffect(() => {
 		if (currentView !== 'database') {
 			firebase.db
 				.collection(currentView + '-database')
+				.orderBy('added', 'desc')
 				.get()
-				.then((queryRes: firebase.firestore.QuerySnapshot) => {
-					let newArr: firebase.firestore.DocumentData[] = [];
-					queryRes.docs.forEach((doc) => {
-						newArr.push(doc.data());
-					});
-					setDataArray(newArr);
+				.then((res: firebase.firestore.QuerySnapshot) => {
+					const resultAsDataArray = createDataArray(res);
+					setDataArray(resultAsDataArray);
 				});
 		} else {
 			let newArr: firebase.firestore.DocumentData[] = [];
 			routes.database.forEach((route) => {
 				newArr.push(route);
 			});
-			setDataArray(newArr)
+			setDataArray(newArr);
 		}
-	}, [firebase.db, currentView]);
+	}, [currentView, firebase.db]);
+
+	useEffect(() => {
+		setDisplayedData(searchFilter(dataArray));
+	}, [dataArray, searchFilter]);
+
 	return (
 		<div className='row align-items-center justify-content-around h-100 mb-3'>
-			<div className="col-12">
+			<div className={currentView === 'database' ? 'col-12' : 'col-3'}>
 				<BackButton
 					currentView={currentView}
 					setCurrentView={setCurrentView}
 				/>
 			</div>
-			{dataArray &&
-				dataArray.map(
-					(propData: firebase.firestore.DocumentData, i) => (
-						<ItemCard
-							propData={propData}
-							i={i}
-							currentView={currentView}
-							setCurrentView={setCurrentView}
-							key={'dataCard' + i}
-						/>
-					)
-				)}
+			{currentView !== 'database' && (
+				<div className='col-9'>
+					<SearchBar
+						currentSearch={currentSearch}
+						setCurrentSearch={setCurrentSearch}
+						setDataArray={setDataArray}
+					/>
+				</div>
+			)}
+			{displayedData.map(
+				(propData: firebase.firestore.DocumentData, i) => (
+					<ItemCard
+						propData={propData}
+						i={i}
+						currentView={currentView}
+						setCurrentView={setCurrentView}
+						key={'dataCard' + i}
+					/>
+				)
+			)}
 		</div>
 	);
 };
---
+++ src/components/elements/SearchBar/SearchBar.tsx
@@ -0,0 +1,23 @@
+import React, { FC, Dispatch, SetStateAction } from 'react';
+import { withFirebase } from '../../Firebase/context';
+
+interface Props {
+	currentSearch: string;
+	setCurrentSearch: Dispatch<SetStateAction<string>>;
+}
+
+const SearchBar: FC<Props> = ({
+	currentSearch,
+	setCurrentSearch,
+}) => {
+	return (
+		<input
+			className='form-control'
+			value={currentSearch}
+			onChange={(e) => setCurrentSearch(e.target.value)}
+			placeholder='Search...'
+		/>
+	);
+};
+
+export default withFirebase(SearchBar);
