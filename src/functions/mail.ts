ac8425ee32076200d37050a40c5d4194c686e4c9
calthejuggler <calthejuggler@gmail.com>
2020-06-20 23:34:27 +0200

    Added goldenclub mail functions

---
+++ src/functions/mail.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { Dispatch, SetStateAction } from 'react';
+export const sendGoldenClubEmail = (
+	toEmail: string,
+	fromName: string,
+	clubID: string,
+	setError: Dispatch<SetStateAction<string | null>>,
+	setSuccess: Dispatch<SetStateAction<boolean>>
+) => {
+	let parcel = {
+		toEmail: toEmail,
+		fromName: fromName,
+		clubID: clubID,
+	};
+	setSuccess(false);
+	setError(null);
+	axios.post('./var/mail/pass-golden-club.php', parcel).then((res) => {
+		if (res.data === 'fields') {
+			setError('One of the required fields was empty...');
+		} else if (res.data === 'email') {
+			setError('The email address that you provided is invalid.');
+		} else if (res.data === 'server') {
+			setError(
+				'There was an issue with the server. Please try again later!'
+			);
+		} else if (res.data === null) {
+			setError(
+				'Weird issues going on! Send an email to cal@objectepisodes.com and let him know about this message. Then, please try again.'
+			);
+		} else {
+			setSuccess(true);
+		}
+	});
+};
