1adfe0673b32bd918e6e4fe89fe09ee91129b4bc
calthejuggler <calthejuggler@gmail.com>
2020-07-04 14:39:24 +0200

    Removed any type

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SFCElement } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
@@ -9,7 +9,7 @@ import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 const fakeProp = 'YO';
 
-ReactDOM.render<any>(
+ReactDOM.render<SFCElement<any>>(
 	<GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_RECAPTCHAKEY}>
 		<HashRouter>
 			<FirebaseContext.Provider value={new Firebase(fakeProp)}>
